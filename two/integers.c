0f66c4ead75460e99ca16f10809b0fc1dc83094f
cponeill <caseyoneill78@hotmail.com>
2016-12-13 12:10:38 -0500

    integers.c

---
+++ two/integers.c
@@ -0,0 +1,14 @@
+/* Adding the hash-include function */
+#include <stdio.h>
+
+void main (void) {
+
+    int a;
+    int b = 3;
+    int c;
+
+    a = 2;
+    c = a + b;
+    printf("The sum of adding %d and %d equals %d.\n", a, b, c);
+
+}
