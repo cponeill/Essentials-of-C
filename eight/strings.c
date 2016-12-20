4ba1f0cf4d497efdeb6f47df58ca286e1a690df6
cponeill <caseyoneill78@hotmail.com>
2016-12-20 16:05:22 -0500

    strings.c

---
+++ eight/strings.c
@@ -0,0 +1,10 @@
+/* Adding hash-include function */
+#include <stdio.h>
+
+void main (void) {
+    int val = 12;
+    char string[50];
+
+    printf(string, "Th value of val is %d.\n", val);
+    printf("%s", string);
+}
